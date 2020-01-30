80bfef28dbc726e8229c1b6ffa5e7b57f7ddef60
PHtorres <58959268+PHtorres@users.noreply.github.com>
2020-01-30 11:58:22 -0300

    dev web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [nome, setNome] = useState('Paulo');
+
+  function mudarNome() {
+    setNome('Pedro');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{nome}</h1>
+      <button onClick={mudarNome}>Mudar nome</button>
+    </>
   );
 }
 
